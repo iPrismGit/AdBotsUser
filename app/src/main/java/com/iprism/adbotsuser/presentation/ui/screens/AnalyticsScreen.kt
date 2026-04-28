1b796d4a7c4f144282615fff5f011005c6b38355
Omprakash <omprakash.mopuri@improve10x.com>
2026-04-28 14:46:08 +0530

    Add Compose preview for AnalyticsScreen
    - Implement `AnalyticsScreenPreview` to allow previewing the screen within the IDE.
    - Add the necessary `androidx.compose.ui.tooling.preview.Preview` import.

--- app/src/main/java/com/iprism/adbotsuser/presentation/ui/screens/AnalyticsScreen.kt
+++ app/src/main/java/com/iprism/adbotsuser/presentation/ui/screens/AnalyticsScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -108,4 +109,10 @@ fun AnalyticsScreen(onNavWalletHistory:() -> Unit, onNavPromotionDetails :(Strin
             color = BLACK
         )
     }
+}
+
+@Composable
+@Preview
+fun AnalyticsScreenPreview() {
+    AnalyticsScreen({}, {})
 }
